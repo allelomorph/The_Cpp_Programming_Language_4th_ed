859466b0614307078efd360d083ee816b1ce13b7
Samuel Pomeroy <1690@holbertonschool.com>
2022-01-18 00:25:18 +0000

    ch03 3-4-4 added notes on C++17 fold expressions

--- 03_A_Tour_of_Cpp-Abstraction_Mechanisms/examples/3-4-4_variadic_templates.cpp
+++ 03_A_Tour_of_Cpp-Abstraction_Mechanisms/examples/3-4-4_variadic_templates.cpp
@@ -38,7 +38,7 @@ void va_print(T arg, Types ...args) {
  * perhaps in the previous example, in the second to last frame, Types is void,
  * which then causes the overloaded va_f1 to be called
  */
-// more in-depth with varaidic templates, and C++17 shortcuts:
+// more in-depth with varaidic templates:
 // https://eli.thegreenplace.net/2014/variadic-templates-in-c/
 template<typename T>
 T va_sum(T arg) {
@@ -51,23 +51,33 @@ T va_sum(T arg, Types ...args) {
 }
 
 
+// note: in C++17, fold expressions were introduced, which can streamline variadic template syntax:
+// template<typename ...Values>
+// auto va_sum(Values const &...values) {
+// 	return (values + ...);
+// }
+// above uses right associativity: v1 + (v2 + v3)
+// simply put ellipsis on the left for left associativity: (... + values)
+// see https://www.fluentcpp.com/2021/03/12/cpp-fold-expressions/
+
+
 int main() {
-	std::cout << "`va_print(1, 2.2, \"test\", false);`: " << std::endl;
+	std::cout << "va_print(1, 2.2, \"test\", false): " << std::endl;
 	va_print(1, 2.2, "test", false);
 	std::cout << std::endl;
 
-	std::cout << "`va_print(0.2, 'C', \"C++\", 3, 2, true);`: " << std::endl;
+	std::cout << "va_print(0.2, 'C', \"C++\", 3, 2, true): " << std::endl;
 	va_print(0.2, 'C', "C++", 3, 2, true);
 	std::cout << std::endl;
 
 	// 2.2 becomes 2 in this example
-	std::cout << "`va_sum(-1, 2.2, 3, true)`: " <<
+	std::cout << "va_sum(-1, 2.2, 3, true): " <<
 		va_sum(-1, 2.2, 3, true) << std::endl;
 
-	std::cout << "`va_sum(1.3, 2.2, true)`: " <<
+	std::cout << "va_sum(1.3, 2.2, true): " <<
 		va_sum(1.3, 2.2, true) << std::endl;
 
-	std::cout << "`va_sum(1, 2, 3)`: " <<
+	std::cout << "va_sum(1, 2, 3): " <<
 		va_sum(1, 2, 3) << std::endl;
 }
 /////////// end file main.cpp
