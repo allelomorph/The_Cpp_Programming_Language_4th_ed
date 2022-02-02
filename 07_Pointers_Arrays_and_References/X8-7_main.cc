1a8d3252a001d53a3b0de2860dcb7f43b8f007bc
Samuel Pomeroy <1690@holbertonschool.com>
2022-02-02 00:15:38 +0000

    ch07 X8-7 upgraded swap() to idiomatic use of rvalues

--- 07_Pointers_Arrays_and_References/X8-7_main.cc
+++ 07_Pointers_Arrays_and_References/X8-7_main.cc
@@ -6,6 +6,7 @@ void IntValueSwap(int *a, int *b) {
 		std::cerr << "IntValueSwap: nullptr parameter"
 			  << std::endl;
 	} else {
+		// C-like swap by copying
 		int temp {*a};
 		*a = *b;
 		*b = temp;
@@ -13,12 +14,16 @@ void IntValueSwap(int *a, int *b) {
 }
 
 
-// one advantage of a reference is that it cannot exist without a target (?),
+// one advantage of a reference is that it cannot exist without a target,
 //   so no need to filter bad inputs here
 void IntValueSwap(int &a, int &b) {
-	int temp {a};
-	a = b;
-	b = temp;
+	// swapping can be done by moving/using rvalue references, versus the
+	//   potentially costly copying/using lvalue references
+	// move() does not actually move, but instead creates a rvalue reference,
+	//   equivalent to static_cast<T&&>(v) for T v
+	int temp {std::move(a)};
+	a = std::move(b);
+	b = std::move(temp);
 }
 
 
