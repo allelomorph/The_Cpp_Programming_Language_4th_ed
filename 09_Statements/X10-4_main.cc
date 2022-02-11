db3225a1b8574ac54b2c9f9a8f82da58e3bb370e
Samuel Pomeroy <1690@holbertonschool.com>
2022-02-11 22:20:34 +0000

    ch09 X10-4 comments, mixed hex case exptn, test cases

--- 09_Statements/X10-4_main.cc
+++ 09_Statements/X10-4_main.cc
@@ -16,7 +16,8 @@ int atoi(const char *s) {
         negative = -1;
         ++s;
     }
-
+    // std::atoi returns 0 for bad numeral strings, so not throwing exceptions
+    //  for "" or naked '-', "0x", or "0X"
     bool cap_hex {false};
     int base {10};
     if (*s == '0') {
@@ -28,15 +29,13 @@ int atoi(const char *s) {
             base = 8;
         }
     }
-
-    // count digits in representation
+    // advance to lowest place digit in representation
     const char *rep_start {s};
     if (base == 16)
         while (std::isxdigit(*s)) ++s;
     else
         while (std::isdigit(*s)) ++s;
     --s;
-
     char rep_ch[17] { "0123456789abcdef" };
     if (cap_hex) {
         for (auto i = 10; rep_ch[i]; ++i)
@@ -44,14 +43,16 @@ int atoi(const char *s) {
     }
     int res {0};
     for (int pow {1}; s - rep_start >= 0; --s, pow *= base) {
-        for (int i {0}; rep_ch[i]; ++i) {
+        int i {0};
+        for (; rep_ch[i]; ++i) {
             if (*s == rep_ch[i]) {
                 res += i * pow;
                 break;
             }
         }
+        if (i == 16)
+            throw std::invalid_argument("X10_3::atoi: mixed hex case");
     }
-
     return negative * res;
 }
 
@@ -59,31 +60,57 @@ int atoi(const char *s) {
 
 
 int main() {
+    try {
+        std::cout << "atoi(nullptr): " << X10_3::atoi(nullptr) << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    try {
+        std::cout << "atoi(\"0xdF\"): " << X10_3::atoi("0xdF") << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << "atoi(\"\"): " << X10_3::atoi("") << std::endl;
+    std::cout << "atoi(\"z\"): " << X10_3::atoi("z") << std::endl;
+    std::cout << "atoi(\"-z\"): " << X10_3::atoi("-z") << std::endl;
+    std::cout << "atoi(\"0z\"): " << X10_3::atoi("0z") << std::endl;
+    std::cout << "atoi(\"0xz\"): " << X10_3::atoi("0xz") << std::endl;
+    std::cout << "atoi(\"0Xz\"): " << X10_3::atoi("0Xz") << std::endl;
+
+    std::cout << "\nReference std::dec << 0: " << 0 << std::endl;
+    std::cout << X10_3::atoi("0") << std::endl;
+    std::cout << X10_3::atoi("-0") << std::endl;
+    std::cout << X10_3::atoi("    0") << std::endl;
+    std::cout << X10_3::atoi("   -0") << std::endl;
+
+    std::cout << "\nReference std::dec << 0x0: " << 0x0 << std::endl;
+    std::cout << X10_3::atoi("0x0") << std::endl;
+    std::cout << X10_3::atoi("-0x0") << std::endl;
+    std::cout << X10_3::atoi("    0x0") << std::endl;
+    std::cout << X10_3::atoi("   -0x0") << std::endl;
+
     std::cout << "\nReference std::dec << 123: " << 123 << std::endl;
     std::cout << X10_3::atoi("123") << std::endl;
     std::cout << X10_3::atoi("-123") << std::endl;
     std::cout << X10_3::atoi("    123") << std::endl;
     std::cout << X10_3::atoi("   -123") << std::endl;
-//    std::cout << X10_3::atoi("   - 123") << std::endl;
 
     std::cout << "\nReference std::dec << 067: " << 067 << std::endl;
     std::cout << X10_3::atoi("067") << std::endl;
     std::cout << X10_3::atoi("-067") << std::endl;
     std::cout << X10_3::atoi("    067") << std::endl;
     std::cout << X10_3::atoi("   -067") << std::endl;
-//    std::cout << X10_3::atoi("   - 067") << std::endl;
 
     std::cout << "\nReference std::dec << 0xdf: " << 0xdf << std::endl;
     std::cout << X10_3::atoi("0xdf") << std::endl;
     std::cout << X10_3::atoi("-0xdf") << std::endl;
     std::cout << X10_3::atoi("    0xdf") << std::endl;
     std::cout << X10_3::atoi("   -0xdf") << std::endl;
-//    std::cout << X10_3::atoi("   - 0xdf") << std::endl;
 
     std::cout << "\nReference std::dec << 0XDF: " << 0XDF << std::endl;
     std::cout << X10_3::atoi("0XDF") << std::endl;
     std::cout << X10_3::atoi("-0XDF") << std::endl;
     std::cout << X10_3::atoi("    0XDF") << std::endl;
     std::cout << X10_3::atoi("   -0XDF") << std::endl;
-//    std::cout << X10_3::atoi("   - 0XDF") << std::endl;
 }
