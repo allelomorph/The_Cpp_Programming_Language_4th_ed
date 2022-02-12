e789d23fdf68b998439cdf136ecd98ec88720528
Samuel Pomeroy <1690@holbertonschool.com>
2022-02-12 03:09:11 +0000

    ch09 X10-6 namespace references

--- 09_Statements/X10-6_main.cc
+++ 09_Statements/X10-6_main.cc
@@ -18,7 +18,7 @@ void _atoiRecursion(int i, char *b, int pow_10, bool negative) {
 
 void itoa(int i, char *b, std::size_t buf_sz) {
     if (b == nullptr)
-        throw std::invalid_argument("X10_5::itoa: null buffer");
+        throw std::invalid_argument("X10_6::itoa: null buffer");
     if (!buf_sz)
         return;
 
@@ -33,7 +33,7 @@ void itoa(int i, char *b, std::size_t buf_sz) {
         pow_10 *= 10;
     }
     if (buf_sz < rep_len)
-        throw std::out_of_range("X10_5::itoa: buffer too small for numeral string");
+        throw std::out_of_range("X10_6::itoa: buffer too small for numeral string");
 
     if (neg)
         *b++ = '-';
@@ -42,8 +42,8 @@ void itoa(int i, char *b, std::size_t buf_sz) {
 
 
 void itoaTestPrint(int i, char *buf, std::size_t buf_sz) {
-    X10_5::itoa(i, buf, buf_sz);
-    std::cout << "X10_5::itoa(" << i << ", buf): \"" << buf << '"' << std::endl;
+    X10_6::itoa(i, buf, buf_sz);
+    std::cout << "X10_6::itoa(" << i << ", buf): \"" << buf << '"' << std::endl;
     std::memset(buf, 0, buf_sz);
 }
 
@@ -56,16 +56,16 @@ int main() {
     char buf[buf_sz] {0};   // max chars == 20 (LONG_MAX, -9223372036854775808)
 
     try {
-        std::cout << "X10_5::itoa(n, nullptr): ";
-        X10_5::itoa(10, nullptr, buf_sz);
+        std::cout << "X10_6::itoa(n, nullptr): ";
+        X10_6::itoa(10, nullptr, buf_sz);
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
 
-    X10_5::itoaTestPrint(0, buf, buf_sz);
-    X10_5::itoaTestPrint(-0, buf, buf_sz);
-    X10_5::itoaTestPrint(123, buf, buf_sz);
-    X10_5::itoaTestPrint(-123, buf, buf_sz);
-    X10_5::itoaTestPrint(std::numeric_limits<int>::min(), buf, buf_sz);
-    X10_5::itoaTestPrint(std::numeric_limits<int>::max(), buf, buf_sz);
+    X10_6::itoaTestPrint(0, buf, buf_sz);
+    X10_6::itoaTestPrint(-0, buf, buf_sz);
+    X10_6::itoaTestPrint(123, buf, buf_sz);
+    X10_6::itoaTestPrint(-123, buf, buf_sz);
+    X10_6::itoaTestPrint(std::numeric_limits<int>::min(), buf, buf_sz);
+    X10_6::itoaTestPrint(std::numeric_limits<int>::max(), buf, buf_sz);
 }
